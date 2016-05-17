b584c2f07e2d1a5d3a283b694fe83d750af4aaa6
vubaquang243 <vubaquang123@gmail.com>
2016-05-17 10:59:31 +0700

    Add files via upload

---
+++ EC_NhaSanXuat.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EntityClass
+{
+    public class EC_NhaSanXuat
+    {
+        private string _mANSX;
+
+        private string _tENNSX;
+
+        public string MANSX
+        {
+            get
+            {
+                return _mANSX;
+            }
+
+            set
+            {
+                _mANSX = value;
+            }
+        }
+
+        public string TENNSX
+        {
+            get
+            {
+                return _tENNSX;
+            }
+
+            set
+            {
+                _tENNSX = value;
+            }
+        }
+    }
+}
